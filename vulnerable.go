edb7ed54d9c7984ebbc6e9e0e1a6d51f71bb854b
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-11-04 10:12:57 -0800

    commit with no tags

---
+++ vulnerable.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"GITHUBTEST/dog"
+)
+
+func main () {
+	fmt.Println ("Package Name: akita")
+	dog.dog.AkitaPackageName()
+	dog.dog.ColliePackageName()
+        type Context = context.Context
+        type CancelFunc = context.CancelFunc
+}
